f7e3633806ee2df92bce2dfafc6b3b5dd3d137e4
jboulouloubi <jamesbechet@gmail.com>
2014-08-12 20:45:49 +0100

    Inline js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-  var s = window.scrollY;
-
-  if (s < 300) {
-    opacity = (s / 150.0);
-    document.getElementsByClassName('blur')[0].style.opacity = opacity;
-  }
-}
